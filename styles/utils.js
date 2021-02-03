de6a3ddf9a78266686cf7eb12a872e702bbbc597
lmarello <leomarello@gmail.com>
2021-02-02 22:52:09 -0300

    utils addOpacityToColor

--- styles/utils.js
+++ styles/utils.js
@@ -1,4 +1,4 @@
 export const addOpacityToColor = (color, opacity) => {
-  const opacityHexa = Math.round(opacity * 255).toString(16);
-  return `${color}${opacityHexa}`;
-};
+  const opacityHexa = Math.round(opacity * 255).toString(16)
+  return `${color}${opacityHexa}`
+}
