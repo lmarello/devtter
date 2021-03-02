7df72c425d62d4eae348c845ceeba34c0a842af9
lmarello <leomarello@gmail.com>
2021-03-02 02:47:47 -0300

    hook useTimeAgo

---
+++ hooks/useTimeAgo.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react"
+
+const DATE_UNITS = [
+  ["day", 86400],
+  ["hour", 3600],
+  ["minute", 60],
+  ["second", 1],
+]
+
+const REFRESH_INTERVAL = {
+  second: 5000,
+  minute: 60 * 1000,
+  hour: 3600 * 1000,
+  day: 86400 * 1000,
+}
+
+const getDateDiffs = (timestamp) => {
+  const now = Date.now()
+  const elapsed = (timestamp - now) / 1000 // dividido 1000 para quitar los ms
+
+  for (const [unit, secondsInUnit] of DATE_UNITS) {
+    if (Math.abs(elapsed) > secondsInUnit || unit === "second") {
+      const value = Math.round(elapsed / secondsInUnit)
+      return { value, unit }
+    }
+  }
+}
+
+const getTimeAgo = (timestamp) => {
+  const { value, unit } = getDateDiffs(timestamp)
+  const rtf = new Intl.RelativeTimeFormat(navigator.language, {
+    style: "long",
+  })
+  return rtf.format(value, unit)
+}
+
+export default function useTimeAgo(timestamp) {
+  const [timeAgo, setTimeAgo] = useState(() => getTimeAgo(timestamp))
+
+  useEffect(() => {
+    let ms = REFRESH_INTERVAL.second
+
+    const interval = setInterval(() => {
+      const timeAgo = getTimeAgo(timestamp)
+      ms = REFRESH_INTERVAL[timeAgo.unit]
+      setTimeAgo(timeAgo)
+    }, ms)
+
+    return () => clearTimeout(interval)
+  }, [timestamp])
+
+  return timeAgo
+}
